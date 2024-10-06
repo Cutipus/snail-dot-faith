c8eb0fe495cd0eded9a77e8453c1dd377566cf05
Mick <cutipus@protonmail.com>
2024-10-06 13:59:18 +0300

    Add TailwindCSS support for slicker looks

--- src/snail_dot_faith/__init__.py
+++ src/snail_dot_faith/__init__.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world() -> str:
-    return render_template("index.html", title="snail.faith", heading="Home")
+    return render_template("index.html", title="Snail Church", heading="Church of Snail")
 
 
 @app.route("/update")
 def update() -> str:
-    return f"🐌 {randint(0, 100)}</p>"
+    return render_template("button_response.html", randnum=str(randint(0, 100)))
